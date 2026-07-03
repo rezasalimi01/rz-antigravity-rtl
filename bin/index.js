de903da7a09349383073ac8d3dc9bbed37208651
Mohammad Mahdi Naderi <mmnaderi.ir@gmail.com>
2026-07-04 02:15:50 +0330

    chore: standardize npm package layout

--- bin/index.js
+++ bin/index.js
@@ -13,7 +13,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const { blue, cyan, green, red, yellow, bold } = picocolors;
 
-const pkgPath = path.join(__dirname, 'package.json');
+const pkgPath = path.join(__dirname, '..', 'package.json');
 const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
 
 function printBanner() {
